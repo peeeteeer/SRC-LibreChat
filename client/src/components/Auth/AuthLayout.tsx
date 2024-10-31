b0f09f5e005325561648d8b090c3bd9a19ec6d00
Peter Rothlaender <peter.rothlaender@ginkgo.com>
2024-10-31 15:14:56 +0100

    add DaimlerTruck Logo

--- client/src/components/Auth/AuthLayout.tsx
+++ client/src/components/Auth/AuthLayout.tsx
@@ -59,8 +59,8 @@ function AuthLayout({
     <div className="relative flex min-h-screen flex-col bg-white dark:bg-gray-900">
       <Banner />
       <BlinkAnimation active={isFetching}>
-        <div className="mt-6 h-10 w-full bg-cover">
-          <img src="/assets/logo.svg" className="h-full w-full object-contain" alt="Logo" />
+        <div className="mt-6 h-64 w-full bg-cover">
+          <img src="/assets/Librechat.png" className="h-full w-full object-contain" alt="Logo" />
         </div>
       </BlinkAnimation>
       <DisplayError />
